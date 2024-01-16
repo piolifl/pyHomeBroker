7eca4b3d325a569e48f5eb6b0e0d2a2cba806bba
piolifl <piolifl@gmail.com>
2024-01-16 10:51:01 -0300

    ajustes para celdas vacias

--- HB_49765_coco/main_pyHB.py
+++ HB_49765_coco/main_pyHB.py
@@ -15,8 +15,8 @@ wb = xw.Book('D:\pyHomeBroker\epgb_pyHB.xlsx')
 shtTest = wb.sheets('HomeBroker')
 shtTickers = wb.sheets('Tickers')
 shtTest.range('U1:V1').value  = 0
-shtTest.range('W1').value  = 1
-shtTest.range('Q2:X29').value  = 0
+shtTest.range('W1').value  = 10
+shtTest.range('Q2:X153').value  = 0
 
 def getOptionsList():
     global allOptions
@@ -144,74 +144,81 @@ def limpio():
     shtTest.range('Y22:AA29').value = ''
 
 def namesArs(nombre,plazo): 
-    if nombre[:2] == 'BA' and (nombre[4:5] == 'D' or nombre[4:5] == 'C'):  # // PBA usd
+    if nombre[:2] == 'BA' and (nombre[4:5] == 'D' or nombre[4:5] == 'C'):
         return 'BA7DD'+plazo
-    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and (nombre[3:4] == 'D' or nombre[3:4] == 'C'):  # // letras usd
-        return nombre[:1]+'18'+nombre[1:3]+plazo
-    elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == 'C'):  # // liquidacion usd
+    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and (nombre[3:4] == 'D' or nombre[3:4] == 'C'):
+        if (nombre[1:2] == 'F' or nombre[1:2] == 'Y'): return nombre[:1]+'18'+nombre[1:3]+plazo
+        else: return nombre[:1]+'18'+nombre[1:3]+plazo
+    elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == 'C'):
         return nombre[:4]+plazo
     else: return nombre[:4]+'O'+plazo
 
 def namesCcl(nombre,plazo): 
-    if nombre[:2] == 'BA' and (nombre[4:5] == 'D' or nombre[4:5] == ' '):  # // PBA usd
+    if nombre[:2] == 'BA' and (nombre[4:5] == 'D' or nombre[4:5] == ' '):
         return 'BA7DC'+plazo
-    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and nombre[4:5] == '4':  # // letras usd
+    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and nombre[4:5] == '4':
         return nombre[:1]+nombre[3:5]+'C'+plazo
-    elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == ' '):  # // liquidacion usd
+    elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == ' '):
         return nombre[:4]+'C'+plazo
     else: return nombre[:4]+'C'+plazo
 
 def namesMep(nombre,plazo): 
-    if nombre[:2] == 'BA' and (nombre[4:5] == 'C' or nombre[4:5] == ' '):  # // PBA usd
+    if nombre[:2] == 'BA' and (nombre[4:5] == 'C' or nombre[4:5] == ' '):
         return 'BA7DD'+plazo
-    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and nombre[4:5] == '4':  # // letras usd
+    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and nombre[4:5] == '4':
         return nombre[:1]+nombre[3:5]+'D'+plazo
-    elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == ' '):  # // liquidacion usd
+    elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == ' '):
         return nombre[:4]+'D'+plazo
     else: return nombre[:4]+'D'+plazo
 
 def cargoPlanilla(dicc):
-    shtTest.range('A22').value = dicc['mepCI'][0]    
-    shtTest.range('Y22').value = dicc['mepCI'][1]
-    shtTest.range('Z22').value = namesArs(dicc['mepCI'][0],' - spot')
-    shtTest.range('AA22').value =namesCcl(dicc['mepCI'][0],' - spot')
-    shtTest.range('A23').value = dicc['mep48'][0]
-    shtTest.range('Y23').value = dicc['mep48'][1]
-    shtTest.range('Z23').value = namesArs(dicc['mep48'][0],' - 48hs')
-    shtTest.range('AA23').value =namesCcl(dicc['mep48'][0],' - 48hs')
-    shtTest.range('A24').value = dicc['cclCI'][0]
-    shtTest.range('Y24').value = dicc['cclCI'][1]
-    shtTest.range('Z24').value = namesArs(dicc['cclCI'][0],' - spot')
-    shtTest.range('AA24').value =namesMep(dicc['cclCI'][0],' - spot')
-    shtTest.range('A25').value = dicc['ccl48'][0]
-    shtTest.range('Y25').value = dicc['ccl48'][1]
-    shtTest.range('Z25').value = namesArs(dicc['ccl48'][0],' - 48hs')
-    shtTest.range('AA25').value =namesMep(dicc['ccl48'][0],' - 48hs')
+    if dicc['mepCI'][1] != 10000:
+        shtTest.range('A22').value = dicc['mepCI'][0]    
+        shtTest.range('Y22').value = dicc['mepCI'][1]
+        shtTest.range('Z22').value = namesArs(dicc['mepCI'][0],' - spot')
+        shtTest.range('AA22').value =namesCcl(dicc['mepCI'][0],' - spot')
+    if dicc['mep48'][1] != 10000:    
+        shtTest.range('A23').value = dicc['mep48'][0]
+        shtTest.range('Y23').value = dicc['mep48'][1]
+        shtTest.range('Z23').value = namesArs(dicc['mep48'][0],' - 48hs')
+        shtTest.range('AA23').value =namesCcl(dicc['mep48'][0],' - 48hs')
+    if dicc['cclCI'][1] != 10000:
+        shtTest.range('A24').value = dicc['cclCI'][0]
+        shtTest.range('Y24').value = dicc['cclCI'][1]
+        shtTest.range('Z24').value = namesArs(dicc['cclCI'][0],' - spot')
+        shtTest.range('AA24').value =namesMep(dicc['cclCI'][0],' - spot')
+    if dicc['ccl48'][1] != 10000:
+        shtTest.range('A25').value = dicc['ccl48'][0]
+        shtTest.range('Y25').value = dicc['ccl48'][1]
+        shtTest.range('Z25').value = namesArs(dicc['ccl48'][0],' - 48hs')
+        shtTest.range('AA25').value =namesMep(dicc['ccl48'][0],' - 48hs')
 
-    shtTest.range('A26').value = dicc['arsCImep'][0]
-    shtTest.range('Y26').value = dicc['arsCImep'][1]
-    shtTest.range('Z26').value = namesMep(dicc['arsCImep'][0],' - spot')
-    shtTest.range('AA26').value =namesCcl(dicc['arsCImep'][0],' - spot')
-    shtTest.range('A27').value = dicc['ars48mep'][0]
-    shtTest.range('Y27').value = dicc['ars48mep'][1]
-    shtTest.range('Z27').value = namesMep(dicc['ars48mep'][0],' - 48hs')
-    shtTest.range('AA27').value =namesCcl(dicc['ars48mep'][0],' - 48hs')
-    shtTest.range('A28').value = dicc['arsCIccl'][0]
-    shtTest.range('Y28').value = dicc['arsCIccl'][1]
-    shtTest.range('Z28').value = namesCcl(dicc['arsCIccl'][0],' - spot')
-    shtTest.range('AA28').value =namesMep(dicc['arsCIccl'][0],' - spot')
-    shtTest.range('A29').value = dicc['ars48ccl'][0]
-    shtTest.range('Y29').value = dicc['ars48ccl'][1]
-    shtTest.range('Z29').value = namesCcl(dicc['ars48ccl'][0],' - 48hs')
-    shtTest.range('AA29').value =namesMep(dicc['ars48ccl'][0],' - 48hs')   
-
+    if dicc['arsCImep'][1] != 100:
+        shtTest.range('A26').value = dicc['arsCImep'][0]
+        shtTest.range('Y26').value = dicc['arsCImep'][1]
+        shtTest.range('Z26').value = namesMep(dicc['arsCImep'][0],' - spot')
+        shtTest.range('AA26').value =namesCcl(dicc['arsCImep'][0],' - spot')
+    if dicc['ars48mep'][1] != 100:
+        shtTest.range('A27').value = dicc['ars48mep'][0]
+        shtTest.range('Y27').value = dicc['ars48mep'][1]
+        shtTest.range('Z27').value = namesMep(dicc['ars48mep'][0],' - 48hs')
+        shtTest.range('AA27').value =namesCcl(dicc['ars48mep'][0],' - 48hs')
+    if dicc['arsCIccl'][1] != 100:
+        shtTest.range('A28').value = dicc['arsCIccl'][0]
+        shtTest.range('Y28').value = dicc['arsCIccl'][1]
+        shtTest.range('Z28').value = namesCcl(dicc['arsCIccl'][0],' - spot')
+        shtTest.range('AA28').value =namesMep(dicc['arsCIccl'][0],' - spot')
+    if dicc['ars48ccl'][1] != 100:
+        shtTest.range('A29').value = dicc['ars48ccl'][0]
+        shtTest.range('Y29').value = dicc['ars48ccl'][1]
+        shtTest.range('Z29').value = namesCcl(dicc['ars48ccl'][0],' - 48hs')
+        shtTest.range('AA29').value =namesMep(dicc['ars48ccl'][0],' - 48hs')   
 
 def ilRulo():
-    print(time.strftime("%H:%M:%S"),"Buscando mejores precios ...",end=" // ")
     celda,pesos,dolar = 46,100,10000
     tikers = {'cclCI':['',dolar],'ccl48':['',dolar],'mepCI':['',dolar],'mep48':['',dolar],'arsCIccl':['',pesos],'ars48ccl':['',pesos],'arsCImep':['',pesos],'ars48mep':['',pesos]}
     
-    for valor in shtTest.range('A46:A146').value:
+    for valor in shtTest.range('A46:A153').value:
         arsM = shtTest.range('AA'+str(celda)).value
         if arsM == None: arsM = 100
         arsC = arsM
@@ -233,11 +240,8 @@ def ilRulo():
                 if arsM > tikers['ars48mep'][1]: tikers['ars48mep'] = [namesArs(valor[:5],' - 48hs'),arsM]
                 if 1000 < mep < tikers['mep48'][1]: tikers['mep48'] = [valor,mep]
         celda +=1
-
     cargoPlanilla(tikers)
     
-    print(time.strftime("%H:%M:%S"),'Done!')
-
 def enviarOrden(tipo=str,symbol=str, price=float, size=int, celda=int):
     symbol = shtTest.range(str(symbol)).value.split()
     mas = shtTest.range('U1').value
@@ -246,7 +250,6 @@ def enviarOrden(tipo=str,symbol=str, price=float, size=int, celda=int):
     precioV = (precio - (mas * 2))
     size *= multiplo
     order = 0
-    
     if shtTest.range('V'+str(int(celda+1))).value == 0: 
         shtTest.range('W'+str(int(celda+1))+':'+'X'+str(int(celda+1))).value = 0
     if tipo.lower() == 'buy': 
@@ -290,12 +293,12 @@ while True:
        if time.strftime("%H:%M:%S") > '17:05:00': salida() 
     except: print("Error al escribir datos, reconectando Excel ... ",time.strftime("%H:%M:%S"))
 
-    if shtTest.range('Q1').value != 1:
+    if shtTest.range('A1').value != 'symbol':
         limpio()
         ilRulo()
-        shtTest.range('Q1').value = 1
+        shtTest.range('A1').value = 'symbol'
         
-    for valor in shtTest.range('P46:U147').value:
+    for valor in shtTest.range('P46:U153').value:
         if valor[1] != 0: # COMPRAR precio BID ___________________________________________________________
             try: 
                 enviarOrden('buy','A'+str((int(valor[0])+1)),'C'+str((int(valor[0])+1)),valor[1],valor[0])
--- test.py
+++ test.py
@@ -7,10 +7,9 @@ import time
 wb = xw.Book('D:\pyHomeBroker\epgb_pyHB.xlsx')
 shtTest = wb.sheets('HomeBroker')
 shtTickers = wb.sheets('Tickers')
-shtTest.range('Q2:X147').value  = 0
+shtTest.range('Q2:X153').value  = 0
 
-
-celda,pesos,dolar = 46,100,2000
+celda,pesos,dolar = 46,100,10000
 tikers = {
         'cclCI':['',dolar],'ccl48':['',dolar],
         'mepCI':['',dolar],'mep48':['',dolar],
@@ -19,67 +18,75 @@ tikers = {
         }
 
 def namesArs(nombre,plazo): 
-    if nombre[:2] == 'BA' and (nombre[4:5] == 'D' or nombre[4:5] == 'C'):  # // PBA usd
+    if nombre[:2] == 'BA' and (nombre[4:5] == 'D' or nombre[4:5] == 'C'):
         return 'BA7DD'+plazo
-    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and (nombre[3:4] == 'D' or nombre[3:4] == 'C'):  # // letras usd
-        return nombre[:1]+'18'+nombre[1:3]+plazo
-    elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == 'C'):  # // liquidacion usd
+    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and (nombre[3:4] == 'D' or nombre[3:4] == 'C'):
+        if (nombre[1:2] == 'F' or nombre[1:2] == 'Y'): return nombre[:1]+'18'+nombre[1:3]+plazo
+        else: return nombre[:1]+'18'+nombre[1:3]+plazo
+    elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == 'C'):
         return nombre[:4]+plazo
     else: return nombre[:4]+'O'+plazo
 
 def namesCcl(nombre,plazo): 
-    if nombre[:2] == 'BA' and (nombre[4:5] == 'D' or nombre[4:5] == ' '):  # // PBA usd
+    if nombre[:2] == 'BA' and (nombre[4:5] == 'D' or nombre[4:5] == ' '):
         return 'BA7DC'+plazo
-    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and nombre[4:5] == '4':  # // letras usd
+    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and nombre[4:5] == '4':
         return nombre[:1]+nombre[3:5]+'C'+plazo
-    elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == ' '):  # // liquidacion usd
+    elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == ' '):
         return nombre[:4]+'C'+plazo
     else: return nombre[:4]+'C'+plazo
 
 def namesMep(nombre,plazo): 
-    if nombre[:2] == 'BA' and (nombre[4:5] == 'C' or nombre[4:5] == ' '):  # // PBA usd
+    if nombre[:2] == 'BA' and (nombre[4:5] == 'C' or nombre[4:5] == ' '):
         return 'BA7DD'+plazo
-    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and nombre[4:5] == '4':  # // letras usd
+    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and nombre[4:5] == '4':
         return nombre[:1]+nombre[3:5]+'D'+plazo
-    elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == ' '):  # // liquidacion usd
+    elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == ' '):
         return nombre[:4]+'D'+plazo
     else: return nombre[:4]+'D'+plazo
 
 def cargoPlanilla(dicc):
-    shtTest.range('A22').value = dicc['mepCI'][0]    
-    shtTest.range('Y22').value = dicc['mepCI'][1]
-    shtTest.range('Z22').value = namesArs(dicc['mepCI'][0],' - spot')
-    shtTest.range('AA22').value =namesCcl(dicc['mepCI'][0],' - spot')
-    shtTest.range('A23').value = dicc['mep48'][0]
-    shtTest.range('Y23').value = dicc['mep48'][1]
-    shtTest.range('Z23').value = namesArs(dicc['mep48'][0],' - 48hs')
-    shtTest.range('AA23').value =namesCcl(dicc['mep48'][0],' - 48hs')
-    shtTest.range('A24').value = dicc['cclCI'][0]
-    shtTest.range('Y24').value = dicc['cclCI'][1]
-    shtTest.range('Z24').value = namesArs(dicc['cclCI'][0],' - spot')
-    shtTest.range('AA24').value =namesMep(dicc['cclCI'][0],' - spot')
-    shtTest.range('A25').value = dicc['ccl48'][0]
-    shtTest.range('Y25').value = dicc['ccl48'][1]
-    shtTest.range('Z25').value = namesArs(dicc['ccl48'][0],' - 48hs')
-    shtTest.range('AA25').value =namesMep(dicc['ccl48'][0],' - 48hs')
+    if dicc['mepCI'][1] != 10000:
+        shtTest.range('A22').value = dicc['mepCI'][0]    
+        shtTest.range('Y22').value = dicc['mepCI'][1]
+        shtTest.range('Z22').value = namesArs(dicc['mepCI'][0],' - spot')
+        shtTest.range('AA22').value =namesCcl(dicc['mepCI'][0],' - spot')
+    if dicc['mep48'][1] != 10000:    
+        shtTest.range('A23').value = dicc['mep48'][0]
+        shtTest.range('Y23').value = dicc['mep48'][1]
+        shtTest.range('Z23').value = namesArs(dicc['mep48'][0],' - 48hs')
+        shtTest.range('AA23').value =namesCcl(dicc['mep48'][0],' - 48hs')
+    if dicc['cclCI'][1] != 10000:
+        shtTest.range('A24').value = dicc['cclCI'][0]
+        shtTest.range('Y24').value = dicc['cclCI'][1]
+        shtTest.range('Z24').value = namesArs(dicc['cclCI'][0],' - spot')
+        shtTest.range('AA24').value =namesMep(dicc['cclCI'][0],' - spot')
+    if dicc['ccl48'][1] != 10000:
+        shtTest.range('A25').value = dicc['ccl48'][0]
+        shtTest.range('Y25').value = dicc['ccl48'][1]
+        shtTest.range('Z25').value = namesArs(dicc['ccl48'][0],' - 48hs')
+        shtTest.range('AA25').value =namesMep(dicc['ccl48'][0],' - 48hs')
 
-    shtTest.range('A26').value = dicc['arsCImep'][0]
-    shtTest.range('Y26').value = dicc['arsCImep'][1]
-    shtTest.range('Z26').value = namesMep(dicc['arsCImep'][0],' - spot')
-    shtTest.range('AA26').value =namesCcl(dicc['arsCImep'][0],' - spot')
-    shtTest.range('A27').value = dicc['ars48mep'][0]
-    shtTest.range('Y27').value = dicc['ars48mep'][1]
-    shtTest.range('Z27').value = namesMep(dicc['ars48mep'][0],' - 48hs')
-    shtTest.range('AA27').value =namesCcl(dicc['ars48mep'][0],' - 48hs')
-    shtTest.range('A28').value = dicc['arsCIccl'][0]
-    shtTest.range('Y28').value = dicc['arsCIccl'][1]
-    shtTest.range('Z28').value = namesCcl(dicc['arsCIccl'][0],' - spot')
-    shtTest.range('AA28').value =namesMep(dicc['arsCIccl'][0],' - spot')
-    shtTest.range('A29').value = dicc['ars48ccl'][0]
-    shtTest.range('Y29').value = dicc['ars48ccl'][1]
-    shtTest.range('Z29').value = namesCcl(dicc['ars48ccl'][0],' - 48hs')
-    shtTest.range('AA29').value =namesMep(dicc['ars48ccl'][0],' - 48hs')  
-
+    if dicc['arsCImep'][1] != 100:
+        shtTest.range('A26').value = dicc['arsCImep'][0]
+        shtTest.range('Y26').value = dicc['arsCImep'][1]
+        shtTest.range('Z26').value = namesMep(dicc['arsCImep'][0],' - spot')
+        shtTest.range('AA26').value =namesCcl(dicc['arsCImep'][0],' - spot')
+    if dicc['ars48mep'][1] != 100:
+        shtTest.range('A27').value = dicc['ars48mep'][0]
+        shtTest.range('Y27').value = dicc['ars48mep'][1]
+        shtTest.range('Z27').value = namesMep(dicc['ars48mep'][0],' - 48hs')
+        shtTest.range('AA27').value =namesCcl(dicc['ars48mep'][0],' - 48hs')
+    if dicc['arsCIccl'][1] != 100:
+        shtTest.range('A28').value = dicc['arsCIccl'][0]
+        shtTest.range('Y28').value = dicc['arsCIccl'][1]
+        shtTest.range('Z28').value = namesCcl(dicc['arsCIccl'][0],' - spot')
+        shtTest.range('AA28').value =namesMep(dicc['arsCIccl'][0],' - spot')
+    if dicc['ars48ccl'][1] != 100:
+        shtTest.range('A29').value = dicc['ars48ccl'][0]
+        shtTest.range('Y29').value = dicc['ars48ccl'][1]
+        shtTest.range('Z29').value = namesCcl(dicc['ars48ccl'][0],' - 48hs')
+        shtTest.range('AA29').value =namesMep(dicc['ars48ccl'][0],' - 48hs')  
 
 shtTest.range('A22:A29').value = ''
 shtTest.range('Y22:AA29').value = ''
