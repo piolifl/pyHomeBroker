d4db63ba8c9456a34571f768f1acee2e951f6c4c
piolifl <piolifl@gmail.com>
2024-01-18 22:14:30 -0300

    ajustes al excel y nombres en main

--- HB_10214_coco/main_pyHB.py
+++ HB_10214_coco/main_pyHB.py
@@ -14,9 +14,10 @@ print(time.strftime("%H:%M:%S"),'Abriendo hoja de Excel ...')
 wb = xw.Book('D:\pyHomeBroker\epgb_pyHB.xlsx')
 shtTest = wb.sheets('HomeBroker')
 shtTickers = wb.sheets('Tickers')
+shtTest.range('T1').value = 'NO'
 shtTest.range('U1:V1').value  = 0
 shtTest.range('W1').value  = 10
-shtTest.range('Q2:X153').value  = 0
+shtTest.range('Q2:X29').value  = 0
 
 def getOptionsList():
     global allOptions
@@ -144,10 +145,10 @@ def limpio():
     shtTest.range('Y22:AA29').value = ''
 
 def namesArs(nombre,plazo): 
-    if nombre[:2] == 'BA' and (nombre[4:5] == 'D' or nombre[4:5] == 'C'):
+    if nombre[:2] == 'BA' :
         return 'BA37D'+plazo
     elif (nombre[:1] == 'X' or nombre[:1] == 'S') and (nombre[3:4] == 'D' or nombre[3:4] == 'C'):
-        if (nombre[1:2] == 'F' or nombre[1:2] == 'Y'): return nombre[:1]+'18'+nombre[1:3]+plazo
+        if (nombre[1:2] == 'F' or nombre[1:2] == 'Y'): return nombre[:1]+'20'+nombre[1:3]+plazo
         else: return nombre[:1]+'18'+nombre[1:3]+plazo
     elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == 'C'):
         return nombre[:4]+plazo
@@ -156,8 +157,9 @@ def namesArs(nombre,plazo):
 def namesCcl(nombre,plazo): 
     if nombre[:2] == 'BA' and (nombre[4:5] == 'D' or nombre[4:5] == ' '):
         return 'BA7DC'+plazo
-    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and nombre[4:5] == '4':
-        return nombre[:1]+nombre[3:5]+'C'+plazo
+    elif (nombre[:1] == 'X' or nombre[:1] == 'S') :
+        if nombre[3:4] == 'D': return nombre[:3]+'C'+plazo
+        else: return nombre[:1]+nombre[3:5]+'C'+plazo
     elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == ' '):
         return nombre[:4]+'C'+plazo
     else: return nombre[:4]+'C'+plazo
@@ -165,8 +167,9 @@ def namesCcl(nombre,plazo):
 def namesMep(nombre,plazo): 
     if nombre[:2] == 'BA' and (nombre[4:5] == 'C' or nombre[4:5] == ' '):
         return 'BA7DD'+plazo
-    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and nombre[4:5] == '4':
-        return nombre[:1]+nombre[3:5]+'D'+plazo
+    elif (nombre[:1] == 'X' or nombre[:1] == 'S') :
+        if nombre[3:4] == 'C': return nombre[:3]+'D'+plazo
+        else: return nombre[:1]+nombre[3:5]+'D'+plazo
     elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == ' '):
         return nombre[:4]+'D'+plazo
     else: return nombre[:4]+'D'+plazo
@@ -215,30 +218,30 @@ def cargoPlanilla(dicc):
         shtTest.range('AA29').value =namesMep(dicc['ars48ccl'][0],' - 48hs')   
 
 def ilRulo():
-    celda,pesos,dolar = 46,100,10000
+    celda,pesos,dolar = 46,1000,0
     tikers = {'cclCI':['',dolar],'ccl48':['',dolar],'mepCI':['',dolar],'mep48':['',dolar],'arsCIccl':['',pesos],'ars48ccl':['',pesos],'arsCImep':['',pesos],'ars48mep':['',pesos]}
     
     for valor in shtTest.range('A46:A153').value:
         arsM = shtTest.range('AA'+str(celda)).value
-        if arsM == None: arsM = 100
+        if arsM == None: arsM = 1000
         arsC = arsM
         ccl = shtTest.range('Z'+str(celda)).value
-        if ccl == None: ccl = 10000
+        if ccl == None: ccl = 0
         mep = ccl
         if (valor[7:8] == 's' or valor[8:9] == 's'):
             if valor[3:4] == 'C' or valor[4:5] == 'C': 
                 if arsC > tikers['arsCIccl'][1]: tikers['arsCIccl'] = [namesArs(valor[:5],' - spot'),arsC]
-                if 1000 < ccl < tikers['cclCI'][1]: tikers['cclCI'] = [valor,ccl]
+                if ccl > tikers['cclCI'][1]: tikers['cclCI'] = [valor,ccl]
             if valor[3:4] == 'D' or valor[4:5] == 'D':
                 if arsM > tikers['arsCImep'][1]: tikers['arsCImep'] = [namesArs(valor[:5],' - spot'),arsM]
-                if 1000 < mep < tikers['mepCI'][1]: tikers['mepCI'] = [valor,mep]
+                if mep > tikers['mepCI'][1]: tikers['mepCI'] = [valor,mep]
         if (valor[7:9]=='48' or valor[8:10]=='48'):
             if valor[3:4] == 'C' or valor[4:5] == 'C': 
                 if arsC > tikers['ars48ccl'][1]: tikers['ars48ccl'] = [namesArs(valor[:5],' - 48hs'),arsC]
-                if 1000 < ccl < tikers['ccl48'][1]: tikers['ccl48'] = [valor,ccl]
+                if ccl > tikers['ccl48'][1]: tikers['ccl48'] = [valor,ccl]
             if valor[3:4] == 'D' or valor[4:5] == 'D': 
                 if arsM > tikers['ars48mep'][1]: tikers['ars48mep'] = [namesArs(valor[:5],' - 48hs'),arsM]
-                if 1000 < mep < tikers['mep48'][1]: tikers['mep48'] = [valor,mep]
+                if mep > tikers['mep48'][1]: tikers['mep48'] = [valor,mep]
         celda +=1
     cargoPlanilla(tikers)
     
@@ -297,7 +300,11 @@ while True:
         limpio()
         ilRulo()
         shtTest.range('A1').value = 'symbol'
-        
+
+    if shtTest.range('T1').value == 'NO': 
+        time.sleep(1)
+        continue
+
     for valor in shtTest.range('P46:U153').value:
         if valor[1] != 0: # COMPRAR precio BID ___________________________________________________________
             try: 
--- test.py
+++ test.py
@@ -7,9 +7,9 @@ import time
 wb = xw.Book('D:\pyHomeBroker\epgb_pyHB.xlsx')
 shtTest = wb.sheets('HomeBroker')
 shtTickers = wb.sheets('Tickers')
-shtTest.range('Q2:X153').value  = 0
+shtTest.range('Q2:X29').value  = 0
 
-celda,pesos,dolar = 46,100,10000
+celda,pesos,dolar = 46,1000,0
 tikers = {
         'cclCI':['',dolar],'ccl48':['',dolar],
         'mepCI':['',dolar],'mep48':['',dolar],
@@ -18,10 +18,10 @@ tikers = {
         }
 
 def namesArs(nombre,plazo): 
-    if nombre[:2] == 'BA' and (nombre[4:5] == 'D' or nombre[4:5] == 'C'):
-        return 'BA7DD'+plazo
+    if nombre[:2] == 'BA' :
+        return 'BA37D'+plazo
     elif (nombre[:1] == 'X' or nombre[:1] == 'S') and (nombre[3:4] == 'D' or nombre[3:4] == 'C'):
-        if (nombre[1:2] == 'F' or nombre[1:2] == 'Y'): return nombre[:1]+'18'+nombre[1:3]+plazo
+        if (nombre[1:2] == 'F' or nombre[1:2] == 'Y'): return nombre[:1]+'20'+nombre[1:3]+plazo
         else: return nombre[:1]+'18'+nombre[1:3]+plazo
     elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == 'C'):
         return nombre[:4]+plazo
@@ -30,8 +30,9 @@ def namesArs(nombre,plazo):
 def namesCcl(nombre,plazo): 
     if nombre[:2] == 'BA' and (nombre[4:5] == 'D' or nombre[4:5] == ' '):
         return 'BA7DC'+plazo
-    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and nombre[4:5] == '4':
-        return nombre[:1]+nombre[3:5]+'C'+plazo
+    elif (nombre[:1] == 'X' or nombre[:1] == 'S') :
+        if nombre[3:4] == 'D': return nombre[:3]+'C'+plazo
+        else: return nombre[:1]+nombre[3:5]+'C'+plazo
     elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == ' '):
         return nombre[:4]+'C'+plazo
     else: return nombre[:4]+'C'+plazo
@@ -39,8 +40,9 @@ def namesCcl(nombre,plazo):
 def namesMep(nombre,plazo): 
     if nombre[:2] == 'BA' and (nombre[4:5] == 'C' or nombre[4:5] == ' '):
         return 'BA7DD'+plazo
-    elif (nombre[:1] == 'X' or nombre[:1] == 'S') and nombre[4:5] == '4':
-        return nombre[:1]+nombre[3:5]+'D'+plazo
+    elif (nombre[:1] == 'X' or nombre[:1] == 'S') :
+        if nombre[3:4] == 'C': return nombre[:3]+'D'+plazo
+        else: return nombre[:1]+nombre[3:5]+'D'+plazo
     elif (nombre[:2] == 'AL' or nombre[:2] == 'GD' or nombre[:2] == 'AE') and (nombre[4:5] == 'D' or nombre[4:5] == ' '):
         return nombre[:4]+'D'+plazo
     else: return nombre[:4]+'D'+plazo
@@ -93,27 +95,27 @@ shtTest.range('Y22:AA29').value = ''
 
 for valor in shtTest.range('A46:A146').value:
         arsM = shtTest.range('AA'+str(celda)).value
-        if arsM == None: arsM = 100
+        if arsM == None: arsM = 1000
         arsC = arsM
         ccl = shtTest.range('Z'+str(celda)).value
-        if ccl == None: ccl = 10000
+        if ccl == None: ccl = 0
         mep = ccl
         if (valor[7:8] == 's' or valor[8:9] == 's'):
             if valor[3:4] == 'C' or valor[4:5] == 'C': 
-                if 1000 < arsC > tikers['arsCIccl'][1]: tikers['arsCIccl'] = [namesArs(valor[:5],' - spot'),arsC]
-                if 1000 < ccl < tikers['cclCI'][1]: tikers['cclCI'] = [valor,ccl]
+                if arsC > tikers['arsCIccl'][1]: tikers['arsCIccl'] = [namesArs(valor[:5],' - spot'),arsC]
+                if ccl > tikers['cclCI'][1]: tikers['cclCI'] = [valor,ccl]
 
             if valor[3:4] == 'D' or valor[4:5] == 'D':
                 if arsM > tikers['arsCImep'][1]: tikers['arsCImep'] = [namesArs(valor[:5],' - spot'),arsM]
-                if 1000 < mep < tikers['mepCI'][1]: tikers['mepCI'] = [valor,mep]
+                if mep > tikers['mepCI'][1]: tikers['mepCI'] = [valor,mep]
 
         if (valor[7:9]=='48' or valor[8:10]=='48'):
             if valor[3:4] == 'C' or valor[4:5] == 'C': 
                 if arsC > tikers['ars48ccl'][1]: tikers['ars48ccl'] = [namesArs(valor[:5],' - 48hs'),arsC]
-                if 1000 < ccl < tikers['ccl48'][1]: tikers['ccl48'] = [valor,ccl]
+                if ccl > tikers['ccl48'][1]: tikers['ccl48'] = [valor,ccl]
             if valor[3:4] == 'D' or valor[4:5] == 'D': 
                 if arsM > tikers['ars48mep'][1]: tikers['ars48mep'] = [namesArs(valor[:5],' - 48hs'),arsM]
-                if 1000 < mep < tikers['mep48'][1]: tikers['mep48'] = [valor,mep]
+                if mep > tikers['mep48'][1]: tikers['mep48'] = [valor,mep]
         celda +=1
 
 print(tikers)
